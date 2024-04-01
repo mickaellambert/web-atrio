e24d86e29bd207b1f5c839f32b6ccf51793f52b7
Mickaël Lambert <mickael.lambert@wildcodeschool.com>
2024-04-01 16:38:49 +0200

    :round_pushpin: Save a person

---
+++ src/Controller/PersonController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\PersonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Person;
+use Doctrine\ORM\EntityManagerInterface;
+use \DateTimeImmutable;
+
+class PersonController extends AbstractController
+{
+    #[Route('/api/people', name: 'save_person', methods: ['POST'])]
+    public function save(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['firstname']) || !isset($data['lastname']) || !isset($data['birthdate'])) {
+            return new Response('Les champs nom, prénom et date de naissance sont obligatoires', Response::HTTP_BAD_REQUEST);
+        }
+
+        $birthdate = \DateTime::createFromFormat('Y-m-d', $data['birthdate']);
+        
+        if (!$birthdate) {
+            return new Response('La date de naissance doit être au format YYYY-MM-DD', Response::HTTP_BAD_REQUEST);
+        }
+
+        if ($birthdate->diff(new \DateTime())->y >= 150) {
+            return new Response('L\'âge maximum autorisé est de 150 ans', Response::HTTP_BAD_REQUEST);
+        }
+
+        $person = new Person();
+        $person->setFirstname($data['firstname']);
+        $person->setLastname($data['lastname']);
+        $person->setBirthdate(DateTimeImmutable::createFromMutable($birthdate));
+
+        $entityManager->persist($person);
+        $entityManager->flush();
+
+        return new Response('Personne enregistrée avec succès', Response::HTTP_CREATED);
+    }
+}
